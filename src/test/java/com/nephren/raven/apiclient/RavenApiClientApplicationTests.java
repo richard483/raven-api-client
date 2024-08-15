bb1f277f05e1faf17e01f330159dc0d9a3324be6
richard483 <richard.william483@gmail.com>
2024-08-15 09:21:53 +0700

    updated test & removed unused dependency

--- src/test/java/com/nephren/raven/apiclient/RavenApiClientApplicationTests.java
+++ src/test/java/com/nephren/raven/apiclient/RavenApiClientApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureWebTestClient
 class RavenApiClientApplicationTests {
   private final WebTestClient webTestClient;
--- src/test/java/com/nephren/raven/apiclient/serviceExample/server/ServerController.java
+++ src/test/java/com/nephren/raven/apiclient/serviceExample/server/ServerController.java
@@ -2,7 +2,7 @@ package com.nephren.raven.apiclient.serviceExample.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -12,7 +12,7 @@ public class ServerController {
   @Autowired
   private ServerService serverService;
 
-  @PatchMapping(path = "/getRequest")
+  @GetMapping(path = "/getRequest")
   public ResponseEntity<Mono<String>> getRequest() {
     return ResponseEntity.ok(serverService.getRequest());
   }
