56a25d18e447af23bbea1aba22ba9b2fb286fa4b
richard483 <richard.william483@gmail.com>
2025-03-12 15:06:06 +0700

    config: updated config for component scan & styling on api body resolver

--- src/main/java/com/nephren/raven/apiclient/reactor/ReactorConfiguration.java
+++ src/main/java/com/nephren/raven/apiclient/reactor/ReactorConfiguration.java
@@ -4,12 +4,14 @@ import com.nephren.raven.apiclient.properties.SchedulerProperties;
 import com.nephren.raven.apiclient.reactor.factory.SchedulerHelperFactoryBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties({
     SchedulerProperties.class
 })
+@ComponentScan(basePackages = {"com.nephren.raven.apiclient"})
 public class ReactorConfiguration {
 
   @Bean
